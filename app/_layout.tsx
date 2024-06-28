293ac5b1dd4b8eb2cfa0fb2abe538d972244f05d
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-28 19:25:01 +0800

    refactor: refactor layout navigator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
-import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from "expo-router";
-import { useEffect } from "react";
-import { StyleSheet } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import Login from './login';
+
+const Stack = createNativeStackNavigator()
 
 export default function RootLayout() {
 
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="login" options={{ headerShown: false }} />
-      </Stack>
-    </ThemeProvider>
+    <NavigationContainer independent={true}>
+      <Stack.Navigator>
+        <Stack.Screen name ="Login" component={Login} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
