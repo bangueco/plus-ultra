871f457a0ca2c6196061cb8fa2a5b6cfd6f04c9a
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-03 13:58:12 +0800

    chore: move development files to src directory

---
+++ src/utils/config.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const config = {
+  databaseUser: process.env.DATABASE_USER || '',
+  databasePassword: process.env.DATABASE_PASSWORD || '',
+  databaseName: process.env.DATABASE_NAME || '',
+  host: process.env.HOST || '',
+  port: process.env.PORT || 3000,
+  secretKey: process.env.SECRET_KEY || '',
+}
+
+export default config
