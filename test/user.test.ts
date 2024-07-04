801ea6bcdc11f889a30892d44ce4133048243d4b
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-04 14:04:40 +0800

    feat: add api testing for registering users

---
+++ test/user.test.ts
@@ -0,0 +1,29 @@
+import { test, describe, expect, } from 'vitest'
+
+import app from '../src/app'
+import supertest from 'supertest'
+import { faker } from '@faker-js/faker'
+
+const api = supertest(app)
+
+describe('api testing for /api/user', () => {
+  test('registering new user from POST: /api/user/register', async () => {
+
+    const newUser = {
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    }
+
+    const response = await api
+      .post('/api/user/register')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /\application\/json/)
+
+    expect(response.status).toBe(201)
+    expect(response.body).toMatchObject(newUser)
+    expect(response.body.username).toBe(newUser.username)
+    expect(response.body.email).toBe(newUser.email)
+  })
+})
