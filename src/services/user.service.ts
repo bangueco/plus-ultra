4a0baa8fa6242e4b4caa21bedb4855796606b4d9
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-06 15:27:38 +0800

    refactor: register user logic

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,21 +1,17 @@
-import { PrismaClient, User } from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
 import bcrypt from 'bcrypt'
 
 const prisma = new PrismaClient()
 
-const registerUser = async (username: string, email: string, password: string): Promise<User | Error> => {
-  try {
-    const newUser = await prisma.user.create({
-      data: {
-        username: username,
-        email: email,
-        password: bcrypt.hashSync(password, 10)
-      }
-    })
-    return newUser
-  } catch (error) {
-    return error as Error
-  }
+const registerUser = async (username: string, email: string, password: string) => {
+  const newUser = await prisma.user.create({
+    data: {
+      username: username,
+      email: email,
+      password: bcrypt.hashSync(password, 10)
+    }
+  })
+  return newUser
 }
 
 // Export as an object
