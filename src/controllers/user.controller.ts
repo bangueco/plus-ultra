9d1b7ffaa27b0ada58074fde680b0eb477937529
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-06 17:17:23 +0800

    refactor: refactor register controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,14 @@
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 import userService from "../services/user.service"
 
-const register = async (request: Request, response: Response) => {
-  const {username, email, password} = request.body
-  const user = await userService.registerUser(username, email, password)
-  response.status(201).json(user)
+const register = async (request: Request, response: Response, next: NextFunction) => {
+  try {
+    const {username, email, password} = request.body
+    const user = await userService.registerUser(username, email, password)
+    return response.status(201).json(user)
+  } catch (error) {
+    return next(error)
+  }
 }
 
 // Export as an object
