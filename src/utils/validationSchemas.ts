c17eb8a0de9028c0fa4368a5565240f42874c521
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-06 17:16:53 +0800

    feat: add custom error messages

--- src/utils/validationSchemas.ts
+++ src/utils/validationSchemas.ts
@@ -3,9 +3,9 @@ import z from 'zod'
 // Validation for user input schema
 
 const UserInputSchema = z.object({
-  username: z.string().min(4),
-  email: z.string().email(),
-  password: z.string().min(8)
+  username: z.string({message: 'Username is required.'}).min(4, {message: 'Username must contain at least 4 characters or more.'}),
+  email: z.string({message: 'Email is required.'}).email(),
+  password: z.string({message: 'Password is required.'}).min(8, {message: 'Password must contain at least 8 characters or more.'})
 })
 
 export {
