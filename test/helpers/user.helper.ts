69db11b778baa80b7cd7473991f3678044b6ffae
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-07 20:52:00 +0800

    feat: add user testing helper

---
+++ test/helpers/user.helper.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+const getUsersFromDB = async () => {
+  const users = await prisma.user.findMany({})
+  return users.map(user => user)
+}
+
+const deleteUsersFromDB = async () => {
+  try {
+    const result = await prisma.user.deleteMany({});
+    return result;
+  } catch (error) {
+    console.error('Error deleting users:', error);
+    throw error; // Re-throw the error to propagate it to the caller
+  }
+}
+
+const userHelper = {
+  getUsersFromDB,
+  deleteUsersFromDB
+}
+
+export default userHelper
