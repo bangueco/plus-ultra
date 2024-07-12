8dbbab173bc3bb9a28ed42332e04eb0c734f5731
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-12 19:04:17 +0800

    refactor: small changes to accomodate file changes

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,12 +1,12 @@
 import express from 'express'
 import userController from '../controllers/user.controller'
 
-import { validateUserRegistration } from '../middlewares/userValidation'
+import validate from '../middlewares/validate'
 
 const userRouter = express.Router()
 
-userRouter.post('/register', validateUserRegistration, userController.register)
+userRouter.post('/register', validate.userRegistration, userController.register)
 
-userRouter.post('/login', userController.login)
-
+userRouter.post('/login', validate.userAuthentication, userController.login)
+                          
 export default userRouter
