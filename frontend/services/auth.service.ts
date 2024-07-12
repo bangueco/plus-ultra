6d8acf61b934985860c51928312f97eed22aee47
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-12 22:08:47 +0800

    fix: base url small changes

--- frontend/services/auth.service.ts
+++ frontend/services/auth.service.ts
@@ -1,15 +1,15 @@
 import axios from "axios"
-const baseURL = 'http://localhost:3000/api/user/register'
+const baseURL = 'http://localhost:3000/api/user'
 
 // Having an undefined parameters here is fine, as the backend will check it anyway and throw an error message
 
 const register = async (username: string | undefined, email: string | undefined, password: string | undefined) => {
-  const request = await axios.post(baseURL, {username, email, password})
+  const request = await axios.post(`${baseURL}/register`, {username, email, password})
   return request.data
 }
 
-const login = async (username: string, password: string) => {
-  const request = await axios.post(baseURL, {username, password})
+const login = async (username: string | undefined, password: string | undefined) => {
+  const request = await axios.post(`${baseURL}/login`, {username, password})
   return request.data
 }
 
