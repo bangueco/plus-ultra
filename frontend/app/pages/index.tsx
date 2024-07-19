7555a703fbe06feb469474d64ab3e064da4bd670
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-19 21:36:24 +0800

    feat: seed database everytime user visits this page

--- frontend/app/pages/index.tsx
+++ frontend/app/pages/index.tsx
@@ -2,9 +2,17 @@ import { StyleSheet, Text, View } from "react-native";
 import CustomPressable from "@/components/custom/CustomPressable";
 import { Link, ParamListBase, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "react-native-screens/lib/typescript/native-stack/types";
+import { useEffect } from "react";
+import { exercisesDatabase } from "@/database";
 
 const Welcome = () => {
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+  // Initialize database here
+
+  useEffect(() => {
+    exercisesDatabase.seed()
+  }, [])
 
   return (
     <View style={style.container}>
