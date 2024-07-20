9803c2778e36853accd733b3a6aab6344abb6a81
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-20 23:30:54 +0800

    feat: fetch exercises from local persistent storage

--- frontend/app/pages/(tabs)/exercise.tsx
+++ frontend/app/pages/(tabs)/exercise.tsx
@@ -1,14 +1,37 @@
 import SearchInput from "@/components/custom/SearchInput"
-import { StyleSheet, Text, View } from "react-native"
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native"
 import CustomPressable from "@/components/custom/CustomPressable"
+import { exercisesDatabase } from "@/database"
+import { useEffect, useState } from "react"
+
+interface ExerciseInterface {
+  id: number
+  name: string
+  description: string
+  equipment: string
+  image_link: string
+  target_muscles: string
+  muscle_group: string
+}
 
 const Exercise = () => {
+
+  const [exercises, setExercises] = useState<Array<ExerciseInterface>>([])
+
+  useEffect(() => {
+    exercisesDatabase.db.getAllAsync<ExerciseInterface>('SELECT * FROM exercise;')
+    .then(data => {
+      setExercises(data)
+    })
+    .catch(error => console.error(error))
+  }, [])
+
   return (
     <View style={style.container}>
       <View style={{marginTop: '15%', padding: 5, marginBottom: 20}}>
         <Text style={{color: 'white', fontSize: 35, textAlign: 'center'}}>Exercises</Text>
       </View>
-      <View style={{gap: 10, padding: 10}}>
+      <View style={{gap: 10, padding: 10, marginBottom: 60}}>
         <View style={{alignSelf: 'flex-start'}}>
           <CustomPressable text="+ Add New Exercise" buttonStyle={{backgroundColor: 'transparent'}} textStyle={{fontSize: 15, color: 'white'}} />
         </View>
@@ -23,7 +46,13 @@ const Exercise = () => {
 const style = StyleSheet.create({
   container: {
     display: 'flex',
-    height: '100%'
+    height: '100%',
+  },
+  exercisesContainer: {
+    gap: 10
+  },
+  muscleGroup: {
+    backgroundColor: 'gray'
   }
 })
 
