ee9f1cd5f5b6436cc100de5932c3c90a4f8b1ed2
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-25 21:54:15 +0800

    feat: add constant values for equipment and muscle group

---
+++ frontend/constants/exercise.ts
@@ -0,0 +1,20 @@
+const equipment = [
+  {label: 'Barbell', value: 'barbell'},
+  {label: 'Dumbbell', value: 'dumbbell'},
+  {label: 'Kettlebell', value: 'kettlebell'},
+  {label: 'Machine', value: 'machine'},
+  {label: 'Cable', value: 'cable'},
+]
+
+const muscle_group = [
+  {label: "Chest", value: 'chest'},
+  {label: "Back", value: 'back'},
+  {label: "Legs", value: 'legs'},
+  {label: "Arms", value: 'arms'},
+  {label: "Core", value: 'core'},
+]
+
+export {
+  equipment,
+  muscle_group
+}
