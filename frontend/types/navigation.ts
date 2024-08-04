a59258b17a69adcf8aa94e6f0820980accced631
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-04 12:16:35 +0800

    feat: add type definition for navigation container

---
+++ frontend/types/navigation.ts
@@ -0,0 +1,19 @@
+type RootNativeStackParamList = {
+  Welcome: undefined;
+  Login: undefined;
+  Register: undefined;
+  Tabs: undefined;
+};
+
+type TabsParamList = {
+  Profile: undefined;
+  Workout: undefined;
+  Scan: undefined;
+  History: undefined;
+  Exercise: undefined;
+}
+
+export {
+  RootNativeStackParamList,
+  TabsParamList
+}
