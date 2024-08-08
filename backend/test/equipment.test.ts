4e7d7b798eb8824861edb10fef39b42501c170bc
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-08 22:48:28 +0800

    feat: update testing for equipment identify api

--- backend/test/equipment.test.ts
+++ backend/test/equipment.test.ts
@@ -1,6 +1,6 @@
 import { test, describe, expect } from 'vitest';
 import path from 'node:path';
-import equipmentService from '../src/services/equipment.service';
+import { identifyEquipment } from '../src/services/equipment.service';
 import serpApiResults from './helpers/google-lens.json'
 
 import app from '../src/app'
@@ -11,7 +11,7 @@ const imagePath = path.resolve(__dirname, './helpers/img/dumbbell.jpg');
 
 describe('unit testing for equipment', () => {
   test('identify equipment ', () => {
-    const equipment = equipmentService.identifyEquipment(serpApiResults)
+    const equipment = identifyEquipment(serpApiResults)
 
     expect(equipment.equipment_name).toBe('dumbbell')
     expect(equipment.count).toBe(39)
@@ -22,5 +22,6 @@ describe('api testing for equipment', () => {
   test('uploading img to be identify', async () => {
     const response = await api.post('/api/equipment/identify').attach('image', imagePath)
     expect(response.status).toBe(200)
-  })
+    expect(response.body.equipment).toBe('dumbbell')
+  }, 8000)
 })
