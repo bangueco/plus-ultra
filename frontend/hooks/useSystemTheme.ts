63bc330f381bc8b9d0ce76cd6d690f2faba27ce8
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-22 23:14:21 +0800

    feat: combine navigation and react native paper theming

--- frontend/hooks/useSystemTheme.ts
+++ frontend/hooks/useSystemTheme.ts
@@ -1,10 +1,27 @@
-import { Dark, Light } from "@/constants/theme";
+import { ReactNavigationDark, ReactNavigationLight, NativePaperDark, NativePaperLight } from "@/constants/theme";
 import { useColorScheme } from "react-native";
 
 const useSystemTheme = () => {
   const currentColorScheme = useColorScheme()
 
-  return (currentColorScheme === 'light') ? Light : Dark
+  const CombinedDefaultTheme = {
+    ...NativePaperLight,
+    ...ReactNavigationLight,
+    colors: {
+      ...NativePaperLight.colors,
+      ...ReactNavigationLight.colors,
+    },
+  };
+  const CombinedDarkTheme = {
+    ...NativePaperDark,
+    ...ReactNavigationDark,
+    colors: {
+      ...NativePaperDark.colors,
+      ...ReactNavigationDark.colors,
+    },
+  };
+
+  return (currentColorScheme === 'light') ? CombinedDefaultTheme : CombinedDarkTheme
 }
 
 export default useSystemTheme
