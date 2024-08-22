13c70c0c7e532d66c92d304a768e17e039c62cb2
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-22 13:42:53 +0800

    style: small changes on welcome page

--- frontend/app/pages/index.tsx
+++ frontend/app/pages/index.tsx
@@ -4,9 +4,11 @@ import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "react-native-screens/lib/typescript/native-stack/types";
 import { useEffect } from "react";
 import { exercisesDatabase } from "@/database";
+import useSystemTheme from "@/hooks/useSystemTheme";
 
 const Welcome = () => {
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+  const {colors} = useSystemTheme()
 
   // Initialize database here
 
@@ -17,8 +19,8 @@ const Welcome = () => {
   return (
     <View style={style.container}>
       <View style={{gap: 5, alignItems: 'center'}}>
-        <CustomPressable text="Get Started" textStyle={{fontSize: 30}} buttonStyle={{width: '40%'}} />
-        <Text style={{color: 'white'}}>
+        <CustomPressable text="Get Started" textStyle={{fontSize: 20}} buttonStyle={{padding: 10, borderRadius: 5, backgroundColor: colors.primary}} />
+        <Text style={{color: colors.text}}>
           Already have account? <Text style={{color: 'skyblue', textDecorationLine: 'underline'}} onPress={() => navigation.replace('Login')}>Login</Text> here.
         </Text>
       </View>
