93c48f73f3e76aeb781fda0bcec3556387eaf0b6
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-23 21:28:36 +0800

    feat: add seperate secret keys for access and refresh token

--- backend/src/utils/lib/token.ts
+++ backend/src/utils/lib/token.ts
@@ -2,15 +2,15 @@ import jwt from 'jsonwebtoken'
 import config from '../config'
 
 const generateAccessToken = (id: number, username: string) => {
-  return jwt.sign({id, username}, config.secretKey, {expiresIn: '10m'})
+  return jwt.sign({id, username}, config.jwtAccessKey, {expiresIn: '10m'})
 }
 
 const generateRefreshToken = (id: number, username: string) => {
-  return jwt.sign({id, username}, config.secretKey, {expiresIn: '30d'})
+  return jwt.sign({id, username}, config.jwtRefreshKey, {expiresIn: '30d'})
 }
 
 const verifyToken = (token: string) => {
-  return jwt.verify(token, config.secretKey)
+  return jwt.verify(token, config.jwtAccessKey)
 }
 
 export {
