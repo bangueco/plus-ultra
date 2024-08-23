cd2d899f46cb34f55020e6807e6ac6eaeb6ad9b6
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-23 12:39:50 +0800

    chore: delete request logger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,13 +4,11 @@ import cors from 'cors'
 import userRouter from './routes/user.routes'
 import errorHandler from './middlewares/errorHandler'
 import equipmentRouter from './routes/equipment.routes'
-import requestLogger from './middlewares/requestLogger'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(requestLogger)
 
 // Routes
 app.use('/api/user', userRouter)
