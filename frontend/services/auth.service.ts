013d6383cd489486aeb8819a342bff9068c96a53
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-23 23:59:45 +0800

    feat: change endpoint

--- frontend/services/auth.service.ts
+++ frontend/services/auth.service.ts
@@ -4,12 +4,12 @@ const baseURL = process.env.EXPO_PUBLIC_API_URL
 // Having an undefined parameters here is fine, as the backend will check it anyway and throw an error message
 
 const register = async (username: string | undefined, email: string | undefined, password: string | undefined) => {
-  const request = await axios.post(`${baseURL}/user/register`, {username, email, password})
+  const request = await axios.post(`${baseURL}/auth/register`, {username, email, password})
   return request.data
 }
 
 const login = async (username: string | undefined, password: string | undefined) => {
-  const request = await axios.post(`${baseURL}/user/login`, {username, password})
+  const request = await axios.post(`${baseURL}/auth/login`, {username, password})
   return request.data
 }
 
