314b389a2727a1af5399c23e90b911b5ae0b5800
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-23 23:56:58 +0800

    feat: add react native paper provider

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -8,6 +8,7 @@ import TabsLayout from './pages/(tabs)/_layout';
 import { RootNativeStackParamList } from '@/types/navigation';
 import { rootNavigationRef } from '@/hooks/useNavigationRef';
 import useSystemTheme from '@/hooks/useSystemTheme';
+import { PaperProvider } from 'react-native-paper';
 
 const Stack = createNativeStackNavigator<RootNativeStackParamList>()
 
@@ -16,13 +17,15 @@ export default function RootLayout() {
   const systemTheme = useSystemTheme()
 
   return (
-    <NavigationContainer theme={systemTheme} independent={true} ref={rootNavigationRef}>
-      <Stack.Navigator>
-        <Stack.Screen name="Welcome" component={Welcome} options={{headerShown: false}} />
-        <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
-        <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
-        <Stack.Screen name="Tabs" component={TabsLayout} options={{headerShown: false}} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider theme={systemTheme}>
+      <NavigationContainer theme={systemTheme} independent={true} ref={rootNavigationRef}>
+        <Stack.Navigator>
+          <Stack.Screen name="Welcome" component={Welcome} options={{headerShown: false}} />
+          <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+          <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
+          <Stack.Screen name="Tabs" component={TabsLayout} options={{headerShown: false}} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   )
 }
