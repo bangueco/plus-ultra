1edd5d2bfec5977309be35ba5ac8c700d13e1420
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-27 22:18:58 +0800

    feat: add sets, reps, and exercise reference id into template item

--- frontend/database/index.ts
+++ frontend/database/index.ts
@@ -55,19 +55,25 @@ class TemplatesDatabase extends Database {
           item_id INTEGER PRIMARY KEY NOT NULL,
           item_name VARCHAR(255) NOT NULL,
           muscleGroup VARCHAR(255) NOT NULL,
+          sets INTEGER NOT NULL,
+          reps INTEGER NOT NULL,
           template_id INTEGER NOT NULL,
-          FOREIGN KEY (template_id) REFERENCES templates(template_id)
+          exercise_id INTEGER NOT NULL,
+          FOREIGN KEY (template_id) REFERENCES templates(template_id),
+          FOREIGN KEY(exercise_id) REFERENCES exercise(id)
         );
 
         INSERT INTO templates(template_name, custom) VALUES ('Push Day', 'false');
         INSERT INTO templates(template_name, custom) VALUES ('Pull Day', 'false');
         INSERT INTO templates(template_name, custom) VALUES ('Leg Day', 'false');
 
-        INSERT INTO template_items(item_name, muscleGroup, template_id)
+        INSERT INTO template_items(item_name, muscleGroup, sets, reps, template_id, exercise_id)
         VALUES 
-          ('Incline Dumbbell Bench Press', 'Chest', 1),
-          ('Flat Dumbbell Bench Press', 'Chest', 1),
-          ('Dumbbell Flyes', 'Chest', 1)
+          ('Incline Dumbbell Bench Press', 'Chest', 3, 12, 1, 2),
+          ('Flat Dumbbell Bench Press', 'Chest', 3, 12, 1, 1),
+          ('Dumbbell Flyes', 'Chest', 3, 12, 1, 6),
+          ('Shoulder Press', 'Shoulders', 3, 12, 1, 14),
+          ('Lateral Raise', 'Shoulders', 3, 12, 1, 15);
       `)
     } catch (error) {
       console.error(error)
