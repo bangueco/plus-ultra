cb36f6525bda245bc21c8b31190f4b1f05dde018
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-27 22:18:26 +0800

    feat: add template types

--- frontend/types/templates.ts
+++ frontend/types/templates.ts
@@ -3,3 +3,13 @@ export type TemplatesType = {
   template_name: string,
   custom: string
 }
+
+export type TemplateItem = {
+  item_id: number,
+  item_name: string,
+  muscleGroup: string,
+  sets: number,
+  reps: number,
+  template_id: number,
+  exercise_id: number,
+}
