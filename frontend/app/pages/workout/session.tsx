70453ab4fd8eaad1850897fcf5093a28a8433db6
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-17 21:25:08 +0800

    feat: add workout info and sets

--- frontend/app/pages/workout/session.tsx
+++ frontend/app/pages/workout/session.tsx
@@ -1,10 +1,13 @@
+import { templatesDatabase } from "@/database";
 import { RootProps, useRootNavigation } from "@/hooks/useRootNavigation";
 import useSystemTheme from "@/hooks/useSystemTheme";
+import { ExerciseSets, TemplateItem } from "@/types/templates";
 import { StackActions } from "@react-navigation/native";
-import { useState } from "react";
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text, TextInput, View } from "react-native";
 import { StyleSheet } from "react-native";
 import { Button, Dialog, IconButton, Portal } from 'react-native-paper';
+import { DataTable, Checkbox } from 'react-native-paper';
 
 export default function WorkoutSession({route}: RootProps) {
 
@@ -12,6 +15,9 @@ export default function WorkoutSession({route}: RootProps) {
 
   const [workoutStarted, setWorkoutStarted] = useState<boolean>(false)
   const [cancelWorkoutVisible, setCancelWorkoutVisible] = useState<boolean>(false)
+  const [templateName, setTemplateName] = useState<string>('')
+  const [templateExercises, setTemplateExercises] = useState<Array<TemplateItem>>([])
+  const [exercisesSets, setExercisesSets] = useState<Array<ExerciseSets>>([])
 
   const onPressStartWorkout = () => {
     return setWorkoutStarted(true)
@@ -29,7 +35,35 @@ export default function WorkoutSession({route}: RootProps) {
     }
   }
 
+  const onChangeWeight = async (id: number, value: string) => {
+    return await templatesDatabase.db.runAsync(`UPDATE exercise_sets SET weight = ${value} WHERE id=${id}`)
+  }
+
+  const onChangeReps = async (id: number, value: string) => {
+    return await templatesDatabase.db.runAsync(`UPDATE exercise_sets SET reps = ${value} WHERE id=${id}`)
+  }
+
   console.log(route.params)
+
+  useEffect(() => {
+    const nameOfTemplate = templatesDatabase.db.getFirstSync<{template_name: string}>(`SELECT template_name 
+      FROM templates WHERE template_id=${route.params.templateId}
+    `)
+
+    const exercisesOfTemplate = templatesDatabase.db.getAllSync<TemplateItem>(`SELECT * FROM template_items 
+      WHERE template_id=${route.params.templateId}
+    `)
+
+    const setsOfExercises = templatesDatabase.db.getAllSync<ExerciseSets>(`SELECT * FROM exercise_sets 
+      WHERE template_id=${route.params.templateId}
+    `)
+
+    if (nameOfTemplate && exercisesOfTemplate && setsOfExercises) {
+      setTemplateName(nameOfTemplate?.template_name)
+      setTemplateExercises(exercisesOfTemplate)
+      setExercisesSets(setsOfExercises)
+    }
+  }, [])
 
   return (
     <View style={styles.container}>
@@ -75,10 +109,58 @@ export default function WorkoutSession({route}: RootProps) {
         </View>
       </View>
       <View style={styles.workoutName}>
-        <Text style={{fontSize: 28, color: systemTheme.colors.text, fontWeight: 'bold'}}>Push Day</Text>
+        <Text style={{fontSize: 28, color: systemTheme.colors.text, fontWeight: 'bold'}}>{templateName}</Text>
       </View>
-      <View style={styles.exercisesContainer}>
-
+      <View style={{height: '60%', borderWidth: 1, padding: 10, borderColor: systemTheme.colors.text, borderRadius: 10}}>
+        <ScrollView contentContainerStyle={styles.exercisesContainer} showsVerticalScrollIndicator={false}>
+          {
+            templateExercises && templateExercises.map((exercise, index) => (
+              <View key={exercise.exercise_id}>
+                <Text style={{color: systemTheme.colors.primary, fontSize: 15}}>{index + 1}. {exercise.item_name}</Text>
+                <DataTable style={{paddingBottom: 30}}>
+                  <DataTable.Header>
+                    <DataTable.Title>Sets</DataTable.Title>
+                    <DataTable.Title>Weight</DataTable.Title>
+                    <DataTable.Title>Reps</DataTable.Title>
+                    <DataTable.Title>Status</DataTable.Title>
+                  </DataTable.Header>
+                  {
+                    exercisesSets && exercisesSets.filter(set => set.item_id === exercise.item_id).map((set, index) => (
+                      <DataTable.Row key={set.id}>
+                        <DataTable.Cell>{index + 1}</DataTable.Cell>
+                        <DataTable.Cell>
+                          <TextInput
+                            keyboardType="numeric"
+                            style={{borderWidth: 1, width: '50%'}}
+                            defaultValue={set.weight.toString()}
+                            onChangeText={(e) => onChangeWeight(set.id, e)}
+                          />
+                        </DataTable.Cell>
+                        <DataTable.Cell>
+                          <TextInput
+                            keyboardType="numeric"
+                            style={{borderWidth: 1, width: '50%'}}
+                            defaultValue={set.reps.toString()}
+                            onChangeText={(e) => onChangeReps(set.id, e)}
+                          />
+                        </DataTable.Cell>
+                        <DataTable.Cell>
+                          <Checkbox
+                            status="unchecked"
+                            uncheckedColor="red"
+                          />
+                        </DataTable.Cell>
+                      </DataTable.Row>
+                    ))
+                  }
+                </DataTable>
+                <Button mode="contained">
+                  Add set
+                </Button>
+              </View>
+            ))
+          }
+        </ScrollView>
       </View>
       <View style={{justifyContent: 'center', padding: 20}}>
         <View>
@@ -127,9 +209,7 @@ const styles = StyleSheet.create({
     paddingBottom: 10
   },
   exercisesContainer: {
-    minHeight: '60%',
-    borderColor: 'red',
-    borderWidth: 1,
-    paddingBottom: 30
+    padding: 20,
+    gap: 20
   }
 })
