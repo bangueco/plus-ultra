3e361834d8fc55177acb7ffb6bc3c0cfc51fd0ea
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-22 23:54:08 +0800

    feat: add template schema

---
+++ frontend/db/schema/template.ts
@@ -0,0 +1,7 @@
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const template = sqliteTable('Template', {
+  template_id: integer('template_id', {mode: "number"}).primaryKey({autoIncrement: true}).notNull(),
+  template_name: text('template_name', {mode: "text"}).notNull(),
+  custom: integer('custom', {mode: "number"}),
+})
