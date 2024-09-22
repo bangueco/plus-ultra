b957b068ca3e077e89fa80a418b87a17b4ad428f
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-22 23:59:24 +0800

    refactor: template creation

--- frontend/services/template.service.ts
+++ frontend/services/template.service.ts
@@ -1,23 +1,26 @@
-import { exercisesDatabase, templatesDatabase } from "@/database";
 import { NewTemplateItem } from "@/types/templates";
+import { template, } from "@/db/schema/template";
+import { db } from "@/lib/drizzleClient";
+import { templateItem } from "@/db/schema/templateItems";
 
-const createTemplate = async (template: NewTemplateItem) => {
-  // initialize creating template
-  const initTemplate = await templatesDatabase.db.runAsync(`
-    INSERT INTO templates(template_name, custom) 
-    VALUES (${template.template_name}, 'true');`
-  )
-
-  // insert all exercises
-  const insertExercises = template.exercises.map(async exercise => {
-    const getMuscleGroup = await exercisesDatabase.db.getFirstAsync<{muscleGroup: string}>(`SELECT muscleGroup FROM exercise WHERE id=${exercise.exercise_id}`)
-    await templatesDatabase.db.runAsync(`
-      INSERT INTO template_items(item_name, muscleGroup, template_id, exercise_id)
-      VALUES ('${template.template_name}', '${getMuscleGroup?.muscleGroup}')`
-    )
+const createTemplate = async (templateInfo: NewTemplateItem) => {
+  console.log('it runs!?')
+  await db.transaction(async (tx) => {
+    const newTemplate = await tx.insert(template).values({
+      template_name: templateInfo.template_name,
+      custom: 1
+    })
+    templateInfo.exercises.map(async exercise => {
+      await tx.insert(templateItem).values({
+        template_item_name: templateInfo.template_name,
+        muscle_group: exercise.muscleGroup,
+        template_id: newTemplate.lastInsertRowId,
+        exercise_id: exercise.exercise_id
+      })
+    })
   })
 }
 
-export {
+export default {
   createTemplate
 }
