3d870f85b695877d31a201ea4199da504a3e1b79
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-23 21:54:29 +0800

    chore: small changes on schema

--- frontend/db/schema/exercise.ts
+++ frontend/db/schema/exercise.ts
@@ -2,9 +2,9 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const exercise = sqliteTable('Exercise', {
   exercise_id: integer('id', {mode: "number"}).primaryKey({autoIncrement: true}).notNull(),
-  exercise_name: text('name', {mode: "text"}).notNull(),
-  exercise_muscle_group: text('muscleGroup', {mode: "text"}).notNull(),
-  exercise_equipment: text('equipment', {mode: "text"}).notNull(),
-  exercise_custom: integer('custom', {mode: "number"}),
-  exercise_instructions: text('instructions', {mode: "text"}),
+  name: text('name', {mode: "text"}).notNull(),
+  muscle_group: text('muscle_group', {mode: "text"}).notNull(),
+  equipment: text('equipment', {mode: "text"}).notNull(),
+  custom: integer('custom', {mode: "number"}).default(0),
+  instructions: text('instructions', {mode: "text"}),
 })
--- frontend/db/schema/template.ts
+++ frontend/db/schema/template.ts
@@ -3,5 +3,5 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 export const template = sqliteTable('Template', {
   template_id: integer('template_id', {mode: "number"}).primaryKey({autoIncrement: true}).notNull(),
   template_name: text('template_name', {mode: "text"}).notNull(),
-  custom: integer('custom', {mode: "number"}),
+  custom: integer('custom', {mode: "number"}).default(0),
 })
