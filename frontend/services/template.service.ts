07d5403ec285961a4e396b42c12ae35f0573ceef
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-23 18:05:47 +0800

    feat: add template fetching, update and delete

--- frontend/services/template.service.ts
+++ frontend/services/template.service.ts
@@ -2,9 +2,13 @@ import { NewTemplateItem } from "@/types/templates";
 import { template, } from "@/db/schema/template";
 import { db } from "@/lib/drizzleClient";
 import { templateItem } from "@/db/schema/templateItems";
+import { eq } from "drizzle-orm";
+
+const getAllTemplate = async () => {
+  return await db.select().from(template)
+}
 
 const createTemplate = async (templateInfo: NewTemplateItem) => {
-  console.log('it runs!?')
   await db.transaction(async (tx) => {
     const newTemplate = await tx.insert(template).values({
       template_name: templateInfo.template_name,
@@ -21,6 +25,19 @@ const createTemplate = async (templateInfo: NewTemplateItem) => {
   })
 }
 
+const updateTemplate = async (templateId: number, templateName: string) => {
+  return await db.update(template).set({
+    template_name: templateName
+  }).where(eq(template.template_id, templateId))
+}
+
+const deleteTemplate = async (templateId: number) => {
+  return await db.delete(template).where(eq(template.template_id, templateId))
+}
+
 export default {
-  createTemplate
+  getAllTemplate,
+  createTemplate,
+  updateTemplate,
+  deleteTemplate
 }
