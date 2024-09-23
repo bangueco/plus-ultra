e15dee78ec6fa34206abe31c5f10765b97a96eeb
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-23 18:06:56 +0800

    refactor: template creation

--- frontend/services/template.service.ts
+++ frontend/services/template.service.ts
@@ -8,20 +8,9 @@ const getAllTemplate = async () => {
   return await db.select().from(template)
 }
 
-const createTemplate = async (templateInfo: NewTemplateItem) => {
-  await db.transaction(async (tx) => {
-    const newTemplate = await tx.insert(template).values({
-      template_name: templateInfo.template_name,
-      custom: 1
-    })
-    templateInfo.exercises.map(async exercise => {
-      await tx.insert(templateItem).values({
-        template_item_name: templateInfo.template_name,
-        muscle_group: exercise.muscleGroup,
-        template_id: newTemplate.lastInsertRowId,
-        exercise_id: exercise.exercise_id
-      })
-    })
+const createTemplate = async (templateName: string) => {
+  return await db.insert(template).values({
+    template_name: templateName
   })
 }
 
