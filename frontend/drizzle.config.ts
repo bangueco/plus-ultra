c5643322f47d0942546253cf350987a5cac2cf86
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-23 21:56:53 +0800

    chore: update dependencies

--- frontend/drizzle.config.ts
+++ frontend/drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'drizzle-kit';
 
 export default {
   schema: './db/schema/*',
-  out: './drizzle',
+  out: './db/migrations',
   dialect: 'sqlite',
   driver: 'expo',
 } satisfies Config;
