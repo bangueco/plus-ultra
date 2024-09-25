78892e13f1adb8396130d87417b38bf7b3fed0a9
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-25 23:42:08 +0800

    refactor: database table seeding

--- frontend/app/pages/index.tsx
+++ frontend/app/pages/index.tsx
@@ -3,10 +3,10 @@ import CustomPressable from "@/components/custom/CustomPressable";
 import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "react-native-screens/lib/typescript/native-stack/types";
 import { useEffect } from "react";
-// import { exercisesDatabase, templatesDatabase } from "@/database";
 import useSystemTheme from "@/hooks/useSystemTheme";
 import * as SecureStore from 'expo-secure-store';
 import { useMigrationHelper } from "@/lib/drizzleClient";
+import seed from "@/db/seed";
 
 const Welcome = () => {
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
@@ -15,14 +15,12 @@ const Welcome = () => {
   // Initialize database here
 
   useEffect(() => {
-    /* const setup = async () => {
-      await templatesDatabase.seed()
-      return await exercisesDatabase.seed()
-    } */
     const user = SecureStore.getItem('user')
     if (user) return navigation.replace('Tabs')
 
-    // setup()
+    seed()
+    .then(() => console.log('Table seeded'))
+    .catch(console.error)
   }, [])
 
   const { success, error } = useMigrationHelper();
