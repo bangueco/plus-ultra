f1e28e424e46428070632ebff1cd6626fc95980e
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-25 21:59:44 +0800

    fix: update type safe of sort muscle group function

--- frontend/hooks/sortByMuscleGroup.ts
+++ frontend/hooks/sortByMuscleGroup.ts
@@ -4,10 +4,10 @@ export default function sortByMuscleGroup (exercises: Array<ExerciseInfo>) {
     let sections: Array<{title: string, data: Array<{id: number, name: string}>}> = []
 
     exercises.map(exercise => {
-        if (sections.some(e => e.title === exercise.muscleGroup)) {
-            sections[sections.findIndex(e => e.title === exercise.muscleGroup)].data.push({id: exercise.id, name: exercise.name})
+        if (sections.some(e => e.title === exercise.muscle_group)) {
+            sections[sections.findIndex(e => e.title === exercise.muscle_group)].data.push({id: exercise.exercise_id, name: exercise.name})
         } else {
-            sections.push({title: exercise.muscleGroup, data: [{id: exercise.id, name: exercise.name}]})
+            sections.push({title: exercise.muscle_group, data: [{id: exercise.exercise_id, name: exercise.name}]})
         }
     })
 
