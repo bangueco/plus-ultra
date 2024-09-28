8cbd30c9ae66bf0c3f7ef1655ca9e256cb8d0f28
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-28 20:27:11 +0800

    feat: small improvements and changes on database seeder

--- frontend/db/seed/index.ts
+++ frontend/db/seed/index.ts
@@ -1,14 +1,28 @@
 import { db } from '@/lib/drizzleClient'
 import exercises from './data/exercises.json'
+import pushDayExercises from './data/pushDayExercises.json'
+import pullDayExercises from './data/pullDayExercises.json'
+import legDayExercises from './data/legDayExercises.json'
 
 import exerciseService from '@/services/exercise.service'
 import templateService from '@/services/template.service'
 import { exercise } from '../schema/exercise'
+import templateItemService from '@/services/templateItem.service'
+import { sql } from 'drizzle-orm'
 
 export default async function seed() {
   try {
-    // Default exercises seed
+    // Clear all table rows
     await exerciseService.deleteAllExercise()
+    await templateService.deleteAllTemplate()
+    await templateItemService.deleteAllTemplateItem()
+
+    // Reset table sequences
+    db.run(sql`DELETE FROM sqlite_sequence WHERE name='Exercise';`)
+    db.run(sql`DELETE FROM sqlite_sequence WHERE name='Template';`)
+    db.run(sql`DELETE FROM sqlite_sequence WHERE name='TemplateItem';`)
+
+    // Insert default dataset
     await Promise.all(exercises.map(async (e) => {
       await db.insert(exercise).values({
         name: e.name,
@@ -20,14 +34,13 @@ export default async function seed() {
       });
     }));
 
-    // Default template seed
-    await templateService.deleteAllTemplate()
-    const pushDay = await templateService.createTemplate('Push Day')
-    const pullDay = await templateService.createTemplate('Pull Day')
-    const legDay = await templateService.createTemplate('Leg Day')
+    const pushDay = await templateService.createTemplate('Push Day', false)
+    const pullDay = await templateService.createTemplate('Pull Day', false)
+    const legDay = await templateService.createTemplate('Leg Day', false)
 
-    // Default template items seed
-    // TODO: Add default template items seed
+    await templateItemService.createTemplateItem(pushDay.lastInsertRowId, ...pushDayExercises)
+    await templateItemService.createTemplateItem(pullDay.lastInsertRowId, ...pullDayExercises)
+    await templateItemService.createTemplateItem(legDay.lastInsertRowId, ...legDayExercises)
   } catch (error: unknown) {
     console.error(error)
   }
