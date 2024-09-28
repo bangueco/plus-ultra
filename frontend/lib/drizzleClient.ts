c835c9464cc1b2a7a493e5f3870ba4d4d0cfb6ca
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-28 20:27:28 +0800

    feat: add drizzle studio helper

--- frontend/lib/drizzleClient.ts
+++ frontend/lib/drizzleClient.ts
@@ -1,11 +1,17 @@
 import { drizzle, ExpoSQLiteDatabase } from "drizzle-orm/expo-sqlite";
 import { openDatabaseSync } from 'expo-sqlite/next';
 import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
+import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
 
 import migrations from '@/db/migrations/migrations';
 
 const expoDb = openDatabaseSync("database");
 export const db = drizzle(expoDb)
+
+export const useDrizzleStudioHelper = () => {
+
+  return useDrizzleStudio(expoDb)
+}
 
 export const useMigrationHelper = () => {
 
