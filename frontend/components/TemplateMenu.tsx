e35c0b72e0efec2878e36faffc001f112cb0dbd6
bangueco <121280104+bangueco@users.noreply.github.com>
2024-10-09 23:29:17 +0800

    feat: small changes on templatemenu component

--- frontend/components/TemplateMenu.tsx
+++ frontend/components/TemplateMenu.tsx
@@ -2,20 +2,19 @@ import { useState } from "react";
 import { Divider, IconButton, Menu } from "react-native-paper";
 
 type TemplateMenuProps = {
-  viewTemplate: () => void;
-  editTemplate: () => void;
-  deleteTemplate: () => void;
+  editTemplate?: () => void;
+  deleteTemplate?: () => void;
 };
 
-export default function TemplateMenu({ viewTemplate, editTemplate, deleteTemplate }: TemplateMenuProps) {
+export default function TemplateMenu({ editTemplate, deleteTemplate }: TemplateMenuProps) {
   const [visible, setVisible] = useState(false);
   const closeMenu = () => setVisible(false);
   const openMenu = () => setVisible(true);
 
-  const handleViewTemplate = () => {
+/*   const handleViewTemplate = () => {
     viewTemplate();
     closeMenu(); // Close the menu after viewing the template
-  };
+  }; */
 
   return (
     <Menu
@@ -28,8 +27,6 @@ export default function TemplateMenu({ viewTemplate, editTemplate, deleteTemplat
         ></IconButton>
       }
     >
-      <Menu.Item onPress={handleViewTemplate} leadingIcon="eye" title="View" />
-      <Divider />
       <Menu.Item onPress={editTemplate} leadingIcon="pen" title="Edit" />
       <Divider />
       <Menu.Item onPress={deleteTemplate} leadingIcon="trash-can" title="Delete" />
