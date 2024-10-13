af49758503d107f4fadfd3f4e96a885f15cf5dc4
bangueco <121280104+bangueco@users.noreply.github.com>
2024-10-13 23:15:01 +0800

    refactor: major changes on database migration

--- frontend/db/migrations/0000_chief_lord_tyger.sql
+++ frontend/db/migrations/0000_chief_lord_tyger.sql
@@ -19,6 +19,26 @@ CREATE TABLE `ExerciseSet` (
 	FOREIGN KEY (`template_id`) REFERENCES `Template`(`template_id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
+CREATE TABLE `History` (
+	`history_id` integer PRIMARY KEY NOT NULL,
+	`template_name` text NOT NULL,
+	`elapsed_time` text NOT NULL,
+	`calories_burned` integer NOT NULL,
+	`date` text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `HistoryExercise` (
+	`history_exercise_id` integer PRIMARY KEY NOT NULL,
+	`history_id` integer NOT NULL,
+	`template_item_id` integer NOT NULL,
+	`template_id` integer NOT NULL,
+	`reps` integer NOT NULL,
+	`weight` integer NOT NULL,
+	FOREIGN KEY (`history_id`) REFERENCES `History`(`history_id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`template_item_id`) REFERENCES `TemplateItem`(`template_item_id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`template_id`) REFERENCES `Template`(`template_id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
 CREATE TABLE `Template` (
 	`template_id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`template_name` text NOT NULL,
