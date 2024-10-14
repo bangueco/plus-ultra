7ffed366acb8172f57ac790d23f37d34cfee19cb
bangueco <121280104+bangueco@users.noreply.github.com>
2024-10-14 23:43:54 +0800

    feat: add global exercise list state

---
+++ frontend/store/useExerciseStore.ts
@@ -0,0 +1,40 @@
+import exerciseService from '@/services/exercise.service'
+import { ExerciseInfo } from '@/types/exercise'
+import { SQLiteRunResult } from 'expo-sqlite'
+import { create } from 'zustand'
+
+type State = {
+  exercise: Array<ExerciseInfo>
+}
+
+type Action = {
+  fetchExercise: () => Promise<void>,
+  addExercise: (exerciseName: string, muscleGroup: string, equipmentName: string) => Promise<SQLiteRunResult>,
+}
+
+export const useExerciseStore = create<State & Action>((set) => ({
+  exercise: [],
+  fetchExercise: async () => {
+    try {
+      const exercises = await exerciseService.getAllExercise()
+      set({exercise: exercises})
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  addExercise: async (exerciseName, muscleGroup, equipmentName) => {
+    const newExercise = await exerciseService.createExercise(exerciseName, muscleGroup, equipmentName)
+
+    set((state) => ({
+      exercise: [...state.exercise, {
+        exercise_id: newExercise.lastInsertRowId,
+        name: exerciseName,
+        muscle_group: muscleGroup,
+        equipment: equipmentName,
+        custom: 1,
+      }]
+    }))
+
+    return newExercise
+  },
+}))
