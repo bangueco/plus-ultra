5f6d5c879ef5027aa03d06ec937d4372a06d73ee
bangueco <121280104+bangueco@users.noreply.github.com>
2024-10-21 22:17:38 +0800

    feat: add age for data validation

--- backend/src/utils/schema.ts
+++ backend/src/utils/schema.ts
@@ -6,7 +6,8 @@ const UserSchema = {
     .max(16, {message: 'Username cannot exceed 17 characters.'}),
   password: z.string({ message: 'Password is required.' })
     .min(8, {message: 'Password must contain atleast 8 characters or more.'}),
-  email: z.string({ message: 'Email is required.' }).email({ message: 'Invalid email format.' })
+  email: z.string({ message: 'Email is required.' }).email({ message: 'Invalid email format.' }),
+  age: z.number({message: 'Age is not a valid number!'}).min(14, {message: 'You must be 14+ years old to use this app.'})
 };
 
 export {
