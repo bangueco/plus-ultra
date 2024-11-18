483c785d37e59a06f52e447653b1f62efb436140
bangueco <121280104+bangueco@users.noreply.github.com>
2024-11-18 23:23:58 +0800

    feat: add email verifier

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -22,6 +22,31 @@ const isEmailVerified = async (request: Request, response: Response, next: NextF
   }
 }
 
+const verifyEmail = async (request: Request, response: Response, next: NextFunction) => {
+
+  const emailToken = request.query.emailToken
+  const username = request.query.username
+
+  try {
+    if (!username) throw new ApiError(HttpStatusCode.BAD_REQUEST, "Invalid username!")
+    if (!emailToken) throw new ApiError(HttpStatusCode.BAD_REQUEST, "Invalid email token verification!")
+
+    const user = await userService.findByUsername(String(username))
+
+    if (!user) throw new ApiError(HttpStatusCode.BAD_REQUEST, "Username not found!")
+
+    if (user.emailToken === String(emailToken)) {
+      await userService.updateUser(user.id, {isEmailValid: true})
+      return response.status(HttpStatusCode.OK).json({message: "Email is now verified!"})
+    } else {
+      return response.status(HttpStatusCode.BAD_REQUEST).json({message: "Cannot verify email."})
+    }
+
+  } catch (error) {
+    return next(error)
+  }
+}
+
 export default {
-  isEmailVerified
+  isEmailVerified, verifyEmail
 }
