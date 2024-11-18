bf0ce2c7ae534de0e85b920fc0595fe82f2d2385
bangueco <121280104+bangueco@users.noreply.github.com>
2024-11-18 18:03:25 +0800

    refactor: small changes on user email verification status

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -2,23 +2,21 @@ import { User } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import { ApiError } from "../utils/error";
 import userService from "../services/user.service";
+import { HttpStatusCode } from "../utils/http";
 
 const isEmailVerified = async (request: Request, response: Response, next: NextFunction) => {
 
   const { id } = request.body as User
 
   try {
-    if (!id) throw new ApiError(400, "User id is not specified!")
+    if (!id) throw new ApiError(HttpStatusCode.BAD_REQUEST, "User id is not specified!")
 
     const user = await userService.findById(id)
 
-    if (!user) throw new ApiError(404, "User not found.")
+    if (!user) throw new ApiError(HttpStatusCode.NOT_FOUND, "User not found.")
 
-    if (user.isEmailValid) {
-      return response.status(200).json({message: "The user's email is verified!"})
-    } else {
-      return response.status(400).json({message: "The user's email is not verified!"})
-    }
+    if (user.isEmailValid) return response.status(HttpStatusCode.OK).json({verified: true})
+    if (!user.isEmailValid) return response.status(HttpStatusCode.OK).json({verified: false})
   } catch (error) {
     return next(error)
   }
