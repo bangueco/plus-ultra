39b1a60c9df7b684f6d8e8048b6135d4e1e74aef
bangueco <121280104+bangueco@users.noreply.github.com>
2024-11-24 23:02:48 +0800

    feat(backend): add fetching and adding personal records

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -5,5 +5,7 @@ const userRouter = express.Router()
 
 userRouter.get('/email/status', userController.isEmailVerified)
 userRouter.get('/email/verify', userController.verifyEmail)
+userRouter.get('/personal-record', userController.getPersonalRecord)
+userRouter.post('/personal-record', userController.newPersonalRecord)
 
 export default userRouter
