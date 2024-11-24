ea784548f4368dfccbec0d3d93e80607cc0314f3
bangueco <121280104+bangueco@users.noreply.github.com>
2024-11-24 23:57:16 +0800

    feat(frontend): display user personal record

--- frontend/app/pages/(tabs)/profile.tsx
+++ frontend/app/pages/(tabs)/profile.tsx
@@ -1,12 +1,15 @@
 import { FontAwesome } from "@expo/vector-icons"
-import { useState } from "react"
-import { Alert, Modal, Pressable, StyleSheet, Text, View } from "react-native"
+import { useEffect, useState } from "react"
+import { Alert, FlatList, Modal, Pressable, StyleSheet, Text, View } from "react-native"
 import { StackActions } from "@react-navigation/native"
 import useSystemTheme from "@/hooks/useSystemTheme"
 import { Button, Searchbar, TextInput } from "react-native-paper"
 import { useRootNavigation } from "@/hooks/useRootNavigation";
 import { useUserStore } from "@/store/useUserStore";
 import personalRecordService from "@/services/personalRecord.service"
+import axios from "axios"
+import { SecureStore } from "@/lib/secureStore"
+import { User } from "@/types/user"
 
 const Profile = () => {
   const systemTheme = useSystemTheme()
@@ -17,8 +20,22 @@ const Profile = () => {
   const [exerciseSet, setExerciseSet] = useState<string>()
   const [exerciseReps, setExerciseReps] = useState<string>()
   const [exerciseWeight, setExerciseWeight] = useState<string>()
-
   const [searchQuery, setSearchQuery] = useState<string>('')
+
+  const [data, setData] = useState<Array<{id: number, exercise_name: string, set: number, reps: number, weight: number, userId: number}>>(
+    [
+      {
+        id: 0,
+        exercise_name: '',
+        reps: 0,
+        set: 0,
+        userId: 0,
+        weight: 0
+      }
+    ]
+  );
+
+  const baseURL = process.env.EXPO_PUBLIC_API_URL
 
   const { user } = useUserStore()
 
@@ -56,6 +73,41 @@ const Profile = () => {
       console.error(error)
     }
   }
+
+  const fetchPersonalRecord = async () => {
+
+    const userInfo = SecureStore.getItem('user')
+
+    if (!userInfo) return
+
+    const parsedUserInfo: User = JSON.parse(userInfo)
+
+    try {
+      // Replace with your API endpoint
+      const response = await axios.get(`${baseURL}/user/personal-record`, {
+        params: {
+          userId: !searchQuery ? parsedUserInfo.id : Number(searchQuery)
+        }
+      })
+      setData(response.data); // Store the data in the state
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPersonalRecord();
+
+    // Optional: If you want to refresh data periodically, you can set an interval
+    const interval = setInterval(() => {
+      console.log(searchQuery)
+      console.log('fetch runs')
+      fetchPersonalRecord();
+    }, 5000); // Refresh data every 5 seconds
+
+    // Cleanup the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
 
   return(
     <View style={styles.container}>
@@ -130,7 +182,7 @@ const Profile = () => {
           <View style={{flex: 1, justifyContent: 'center', padding: 5}}>
           <Searchbar
           placeholder="Search for user id"
-          onChangeText={(e) => setSearchQuery(e)}
+          onChangeText={setSearchQuery}
           value={searchQuery}
         />
           </View>
@@ -142,6 +194,23 @@ const Profile = () => {
           >
             Add Workout
           </Button>
+        </View>
+        <View>
+        <FlatList
+          data={data}  // Pass updated data to FlatList
+          keyExtractor={(item) => item.id.toString()}  // Adjust based on your API response
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+              <Text style={styles.itemText}>Exercise Name: {item.exercise_name}</Text>
+              <Text style={styles.itemText}>Sets: {item.set}</Text>
+              <Text style={styles.itemText}>Reps: {item.reps}</Text>
+              <Text style={styles.itemText}>Weight: {item.weight}</Text>
+            </View>
+          )}
+          ListEmptyComponent={
+            <Text style={styles.noResultsText}>No results found</Text>
+          }
+        />
         </View>
       </View>
     </View>
@@ -155,6 +224,20 @@ const styles = StyleSheet.create({
   modalStyle: {
     width: '70%',
     borderRadius: 15,
+  },
+  item: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#f9f9f9',
+    borderRadius: 5,
+  },
+  itemText: {
+    fontSize: 16,
+  },
+  noResultsText: {
+    textAlign: 'center',
+    color: 'gray',
+    fontSize: 16,
   },
 })
 
